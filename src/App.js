5ab639ea342ff75ac76f6759421dfaf6fa169a54
Cezarijus Kivylius <ltcezaris@gmail.com>
2022-06-14 23:47:06 +0100

    add more flexability

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import "./App.css";
 import { useState } from "react";
-import { reduce, expand, pack, unpack } from "./lib";
+import {
+  reduce,
+  expand,
+  pack,
+  unpack,
+  get_reduced_sdp,
+  get_expanded_sdp,
+} from "./lib";
 // import { pack, unpack } from "sdp-minify";
 import * as sdpTransform from "sdp-transform";
 
@@ -9,7 +16,7 @@ import * as sdpTransform from "sdp-transform";
 // unzip(sdpZipString: string) => RTCobj
 const getVerson = (version) => {
   switch (version) {
-    case "sdpm":
+    case "minimal-webrtc":
       return {
         zip: (RTCobj) => reduce(RTCobj),
         unzip: (sdpZipString) => expand(sdpZipString),
@@ -19,17 +26,29 @@ const getVerson = (version) => {
         zip: (RTCobj) => pack(RTCobj, "UTF16"),
         unzip: (sdpZipString) => unpack(sdpZipString, "UTF16"),
       };
+    case "minisdp":
+      return {
+        zip: (RTCobj) => get_reduced_sdp(RTCobj),
+        unzip: (sdpZipString) => get_expanded_sdp(sdpZipString),
+      };
     default:
       return {};
   }
 };
 
-var configuration = {
-  iceServers: [{ urls: "stun:stun.1.google.com:19302" }],
+const genWebRTC = async () => {
+  const configuration = {
+    iceServers: [{ urls: "stun:stun.1.google.com:19302" }],
+  };
+  const peerConnection = new RTCPeerConnection(configuration);
+  const sendChannel = peerConnection.createDataChannel("sendChannel");
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  return { offer, sendChannel };
 };
 
 const App = () => {
-  const [version, setVersion] = useState("sdpm");
+  const [version, setVersion] = useState("minimal-webrtc");
   const { zip, unzip } = getVerson(version);
 
   const [zipValue, setZipValue] = useState({ sdp: "" });
@@ -46,7 +65,12 @@ const App = () => {
   const [unzipValue, setUnzipValue] = useState("");
   const onChangeUnzip = (e) => {
     const { value } = e.target;
-    setUnzipValue(value);
+    try {
+      unzip(value);
+      setUnzipValue(value);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   const onChangeVersion = (e) => {
@@ -58,57 +82,47 @@ const App = () => {
 
   const onClickGen = async (e) => {
     e.preventDefault();
-    const configuration = {
-      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
-    };
-    const peerConnection = new RTCPeerConnection(configuration);
-    const iceCandidates = [];
-    peerConnection.onicecandidate = (e) => {
-      console.log({ onicecandidate: true, e });
-      if (e.candidate) {
-        iceCandidates.push(e.candidate);
-      }
-    };
-
-    peerConnection.ontrack = (e) => {
-      console.log({ ontrack: true, e });
-    };
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
+    const { offer } = await genWebRTC();
     console.log({ offer });
-
-    // const peerConnection2 = new RTCPeerConnection(configuration);
-    // peerConnection2.setRemoteDescription(new RTCSessionDescription(offer));
-    // const answer = await peerConnection2.createAnswer();
-    // await peerConnection2.setLocalDescription(answer);
-    // console.log({ answer: answer });
-    // const pc = new RTCPeerConnection(configuration);
-    // const offer = await pc.createOffer();
-    // pc.setLocalDescription(offer);
-    // const channel = pc.createDataChannel("sendChannel");
-    // channel.onopen = (...params) => console.log({ open: 1, params });
-    // channel.onclose = (...params) => console.log({ close: 1, params });
-    // // const offer = await pc.createOffer();
-    // // pc.setLocalDescription(offer);
-    // // console.log({ offer });
     setZipValue(offer);
   };
+
+  const onClickGenString = async (e) => {
+    e.preventDefault();
+    const { offer } = await genWebRTC();
+    console.log({ offer });
+    setUnzipValue(zip(offer));
+  };
+
+  const outputString = zipValue.sdp ? zip(zipValue) : "";
+  const outputCompresion = zipValue.sdp
+    ? (100 - (outputString.length / zipValue.sdp.length) * 100).toFixed(1)
+    : "--";
+  const jsonOutput = unzipValue ? unzip(unzipValue) : undefined;
 
   return (
     <div>
       <h1>SDPM</h1>
+      <p>
+        In the demo bellow you can pack (compress) and unpack sdp connection
+        strings. You can also generate them if you dont already have one.
+      </p>
       <div>
         Version:{" "}
         <select onChange={onChangeVersion}>
-          <option value="sdpm">sdpm</option>
+          <option disabled value="sdpm">
+            sdpm (TODO) ⭐
+          </option>
+          <option value="minimal-webrtc">minimal-webrtc</option>
           <option value="sdp-minify">sdp-minify</option>
+          <option value="minisdp">minisdp</option>
         </select>
         <br />
         <br />
       </div>
       <div className="container">
         <div className="zip">
-          <h2>Zip</h2>
+          <h2>Pack</h2>
           <label>
             WebRTC JSON (
             <a href="#gen1" onClick={onClickGen}>
@@ -120,12 +134,18 @@ const App = () => {
             onChange={onChangeZip}
             value={zipValue.sdp ? JSON.stringify(zipValue) : ""}
           ></textarea>
+          <small>
+            Length: {(zipValue.sdp ? JSON.stringify(zipValue) : "").length}
+          </small>
           <br />
           <br />
           <label>WebRTC String Output</label>
           <br />
           <input disabled value={zipValue.sdp ? zip(zipValue) : ""}></input>
-          <small>Length: {zipValue.sdp.length} chars</small>
+          <small>
+            Length: {(zipValue.sdp ? zip(zipValue) : "").length} (
+            {outputCompresion}% compresion)
+          </small>
           <hr />
           <h4>Debugger:</h4>
           <pre>
@@ -138,25 +158,30 @@ const App = () => {
         </div>
 
         <div className="unzip">
-          <h2>UNZip</h2>
-          <label>WebRTC String</label>
+          <h2>Unpack</h2>
+          <label>
+            WebRTC String (
+            <a href="#gen2" onClick={onClickGenString}>
+              generate
+            </a>
+            )
+          </label>
           <br />
-          <input onChange={onChangeUnzip}></input>
-          <small>Length: {unzipValue.length} chars.</small>
+          <input onChange={onChangeUnzip} value={unzipValue}></input>
+          <small>Length: {unzipValue.length}</small>
           <br />
           <br />
           <label>WebRTC JSON</label>
           <textarea
             disabled
-            value={unzipValue ? JSON.stringify(unzip(unzipValue)) : undefined}
+            value={unzipValue ? JSON.stringify(jsonOutput) : undefined}
           ></textarea>
+          <small>Length: {jsonOutput ? jsonOutput.sdp.length : "0"}</small>
           <hr />
           <h4>Debugger:</h4>
           <pre>
             {JSON.stringify(
-              unzipValue
-                ? sdpTransform.parse(unzip(unzipValue).sdp)
-                : undefined,
+              unzipValue ? sdpTransform.parse(jsonOutput.sdp) : undefined,
               null,
               4
             )}
@@ -168,3 +193,64 @@ const App = () => {
 };
 
 export default App;
+
+// const configuration = {
+//   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+// };
+// const peerConnection = new RTCPeerConnection();
+// const sendChannel = peerConnection.createDataChannel("sendChannel");
+// const offer = await peerConnection.createOffer();
+// await peerConnection.setLocalDescription(offer);
+// console.log({ offer, sendChannel });
+// setZipValue(offer);
+// const iceCandidates = [];
+
+// const onicecandidate = (e) => {
+//   console.log({ onicecandidate: true, e });
+//   // if (e.candidate) {
+//   //   iceCandidates.push(e.candidate);
+//   // }
+//   if (!e.candidate) {
+//     console.log(
+//       "onicecandidate.offer",
+//       peerConnection.localDescription.sdp
+//     );
+//   } else {
+//     var cand = e.candidate.candidate.split(" ");
+//     console.log(cand[0]);
+//   }
+// };
+// peerConnection.onicecandidate = onicecandidate;
+
+// const ontrack = (e) => {
+//   console.log({ ontrack: true, e });
+// };
+// peerConnection.ontrack = ontrack;
+
+// const sendChannel = peerConnection.createDataChannel("sendDataChannel");
+// const onSendChannelStateChange = () => {
+//   var readyState = sendChannel.readyState;
+//   console.log("ready stat", readyState, sendChannel);
+// };
+// sendChannel.onopen = onSendChannelStateChange;
+// sendChannel.onclose = onSendChannelStateChange;
+
+// const offer = await peerConnection.createOffer();
+// await peerConnection.setLocalDescription(offer);
+// console.log("--" + offer.sdp);
+
+// const peerConnection2 = new RTCPeerConnection(configuration);
+// peerConnection2.setRemoteDescription(new RTCSessionDescription(offer));
+// const answer = await peerConnection2.createAnswer();
+// await peerConnection2.setLocalDescription(answer);
+// console.log({ answer: answer });
+// const pc = new RTCPeerConnection(configuration);
+// const offer = await pc.createOffer();
+// pc.setLocalDescription(offer);
+// const channel = pc.createDataChannel("sendChannel");
+// channel.onopen = (...params) => console.log({ open: 1, params });
+// channel.onclose = (...params) => console.log({ close: 1, params });
+// // const offer = await pc.createOffer();
+// // pc.setLocalDescription(offer);
+// // console.log({ offer });
+// setZipValue(offer);
--- src/lib.js
+++ src/lib.js
@@ -282,8 +282,191 @@ const unpack = (packed) => {
   };
 };
 
-export { reduce, expand, pack, unpack };
+// ------------
 
-// const sdpm = (config) => {
-//   return "some string";
-// };
+function get_reduced_sdp(desc) {
+  var sdp = desc.sdp;
+  var lines = sdp.split("\r\n");
+  var ice_pwd = "";
+  var ice_ufrag = "";
+  var ip_s = new Array();
+  var finger;
+  var type = desc.type === "offer" ? "O" : "A";
+  //console.log(type);
+  for (var i = 0; i < lines.length; i++) {
+    var temp = lines[i].split(":");
+    if (temp[0] == "a=ice-pwd") {
+      ice_pwd = temp[1];
+      //console.log('ice_pwd '+ice_pwd);
+    } else if (temp[0] == "a=ice-ufrag") {
+      ice_ufrag = temp[1];
+      //console.log('ice_ufrag: '+ice_ufrag);
+    } else if (temp[0] == "a=candidate") {
+      if (temp[1].indexOf("raddr") !== -1) {
+        var temp_split = temp[1].split(" ");
+        for (var j = 0; j < temp_split.length; j++) {
+          if (temp_split[j].indexOf(".") !== -1) {
+            if (temp_split[j + 1] == "rport") {
+              //console.log(temp_split[j] + " " + temp_split[j+2]);
+              var t_arr = new Array([
+                encode_ip(temp_split[j]),
+                base32encode(temp_split[j + 2]),
+              ]);
+              ip_s.push(
+                encode_ip(temp_split[j]) + ":" + base32encode(temp_split[j + 2])
+              );
+            } else {
+              var t_arr = new Array([
+                encode_ip(temp_split[j]),
+                base32encode(temp_split[j + 1]),
+              ]);
+              ip_s.push(
+                encode_ip(temp_split[j]) + ":" + base32encode(temp_split[j + 1])
+              );
+              //console.log(temp_split[j] + " " + temp_split[j+1]);
+            }
+          }
+        }
+      }
+      // ice_ufrag = temp[1];
+      // console.log('ice_ufrag: '+ice_ufrag);
+    } else if (temp[0] == "a=fingerprint") {
+      var f = lines[i].split(" ")[1].split(":");
+      var hex = f.map(function (h) {
+        return parseInt(h, 16);
+      });
+      //console.log('hex: '+hex);
+      finger = btoa(String.fromCharCode.apply(String, hex));
+      console.log(finger);
+
+      // var new_finger = atob(finger).split('').map(function (c) { var d = c.charCodeAt(0); var e = c.charCodeAt(0).toString(16).toUpperCase(); if (d < 16) e = '0' + e; return e; }).join(':');
+      // console.log("new_finger " + new_finger);
+      //console.log(f);
+    }
+  }
+  var resp = type + "," + ice_ufrag + "," + ice_pwd + "," + finger;
+  for (var k = 0; k < ip_s.length; k++) {
+    resp += "," + ip_s[k];
+  }
+  //console.log("length: "+ resp.length);
+
+  console.log(resp);
+  console.log("Length: " + resp.length);
+  //console.log(lines);
+  //console.log(ip_s);
+  return resp;
+}
+
+function get_expanded_sdp(red) {
+  var things = red.split(",");
+  console.log("things", things);
+  var type = things[0] === "O" ? "offer" : "answer";
+  var ice_ufrag = things[1];
+  var ice_pwd = things[2];
+  var finger = atob(things[3])
+    .split("")
+    .map(function (c) {
+      var d = c.charCodeAt(0);
+      var e = c.charCodeAt(0).toString(16).toUpperCase();
+      if (d < 16) e = "0" + e;
+      return e;
+    })
+    .join(":");
+  console.log("type: " + type);
+  console.log("ice_ufrag: " + ice_ufrag);
+  console.log("ice_pwd: " + ice_pwd);
+  console.log("fingerprint: " + finger);
+  var ip1 = (things[4] || "0.0.0.0:0").split(":");
+  var glob_ip = decode_ip(ip1[0]);
+  var glob_port = base32decode(ip1[1]);
+
+  console.log("glob_ip: " + glob_ip);
+  console.log("glob_port: " + glob_port);
+
+  var ip2 = (things[5] || "0.0.0.0:0").split(":");
+  var loc_ip = decode_ip(ip2[0]);
+  var loc_port = base32decode(ip2[1]);
+
+  console.log("loc_ip: " + loc_ip);
+  console.log("loc_port: " + loc_port);
+
+  var sdp = [
+    "v=0",
+    "o=- 5498186869896684180 2 IN IP4 127.0.0.1",
+    "s=-",
+    "t=0 0",
+    "a=msid-semantic: WMS",
+    "m=application 9 DTLS/SCTP 5000",
+    "c=IN IP4 " + glob_ip,
+    "a=mid:data",
+    "a=sctpmap:5000 webrtc-datachannel 1024",
+  ];
+
+  if (type === "answer") {
+    sdp.push("a=setup:active");
+  } else {
+    sdp.push("a=setup:actpass");
+  }
+
+  sdp.push("a=ice-ufrag:" + ice_ufrag);
+  sdp.push("a=ice-pwd:" + ice_pwd);
+  sdp.push("a=fingerprint:sha-256 " + finger);
+
+  sdp.push(
+    "a=candidate:328666875 1 udp 2122260223 " +
+      loc_ip +
+      " " +
+      loc_port +
+      " typ host generation 0"
+  );
+  sdp.push(
+    "a=candidate:1561653771 1 tcp 1518280447 " +
+      loc_ip +
+      " 0 typ host tcptype active generation 0"
+  );
+  sdp.push(
+    "a=candidate:3133702446 1 udp 1686052607 " +
+      glob_ip +
+      " " +
+      glob_port +
+      " typ srflx raddr " +
+      loc_ip +
+      " rport " +
+      loc_port +
+      " generation 0"
+  );
+  //console.log(sdp);
+  return { type: type, sdp: sdp.join("\r\n") + "\r\n" };
+}
+function d2h(d) {
+  var temp = d.toString(16);
+  if (d < 16) {
+    return "0" + temp;
+  } else {
+    return temp;
+  }
+}
+function encode_ip(ip) {
+  var temp = ip.split(".");
+  var ans = "";
+  for (var i = 0; i < temp.length; i++) {
+    ans = ans + d2h(parseInt(temp[i]));
+  }
+  return ans;
+}
+function decode_ip(ip) {
+  var ret = new Array();
+  for (var i = 0; i < ip.length / 2; i++) {
+    var temp = ip.substring(i * 2, (i + 1) * 2);
+    ret.push(parseInt(temp, 16));
+  }
+  return ret.join(".");
+}
+function base32encode(num) {
+  return parseInt(num).toString(32);
+}
+function base32decode(num) {
+  return parseInt(num, 32);
+}
+
+export { reduce, expand, pack, unpack, get_reduced_sdp, get_expanded_sdp };
