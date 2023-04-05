10aafd4d03a1792ea794ddd3ca128a3c0b296cab
Cezarijus Kivylius <ltcezaris@gmail.com>
2023-04-05 20:55:58 +0100

    update exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,7 @@
-export { packV0, unpackV0 } from "./v0";
+import { packV0, unpackV0 } from "./v0";
+
+// tempory untill we get v1 working properly
+const pack = packV0;
+const unpack = unpackV0;
+
+export { pack, unpack, packV0, unpackV0 };
